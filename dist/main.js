464f0f935f3e7b1d958375bcde9c989ed27c1ba5
badgerrc@gmail.com <robertchatterson1@gmail.com>
2023-08-27 17:42:15 -0400

    removing obnoxious logs

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,7 @@ var roleUpgrader = require('role.upgrader');
 module.exports.loop = function () {
 
     var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-    console.log('Harvesters: ' + harvesters.length);
+    //console.log('Harvesters: ' + harvesters.length);
 
     if(harvesters.length < 2) {
         var newName = 'Harvester' + Game.time;
